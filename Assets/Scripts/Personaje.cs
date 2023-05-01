4e3261c3c245d1e37881d81ad0b419226943c797
CristopherNicolas <85201838+CristopherNicolas@users.noreply.github.com>
2023-04-30 22:54:54 -0400

    v 1.0

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 [RequireComponent(typeof(AudioSource))]
 public class Personaje : MonoBehaviour
 {
     public float velocidad;
+    PlayerInput xrInput;
     public AudioSource audioSourceJugador;
     bool estaEnZonaSegura = true;
     public void Start()
     {
+        xrInput = GetComponent<PlayerInput>();
         audioSourceJugador = GetComponent<AudioSource>();
     }
     // agacharse
+    private void Update()
+    {
+        if (xrInput.actions["Activate"].IsPressed())
+        {
+            Debug.Log("xr input presionado");
+        }
+    }
     public void Agacharse()
     {
         // el input debe mantenerse para mantenerse agachado
