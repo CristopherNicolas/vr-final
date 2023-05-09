817cb6ff182e1c589f4cd01d4bd37990b6cb62b2
CristopherNicolas <85201838+CristopherNicolas@users.noreply.github.com>
2023-05-09 17:13:04 -0400

    1.2

--- Assets/Bengala.cs
+++ Assets/Bengala.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Bengala : MonoBehaviour
 {
-    public bool estaTomada;
+    public bool estaTomada=false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Direct Interactor")
+        if (other.gameObject.name == "Direct Interactor" && !estaTomada)
         {
             estaTomada = true;
             var player = GameObject.FindObjectOfType<Personaje>();
             player.bengalas.Add(this);
             other.transform.SetParent(other.transform);
+
         }
     }
+        public IEnumerator Consumir()
+        {
+            yield break;
+        }
 }
--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -25,6 +25,7 @@ public class Personaje : MonoBehaviour
             {
                 // soltar bengala
                 bengalas.ForEach(x => x.transform.SetParent(null));
+                //bengalas.
             }
         }
     }
